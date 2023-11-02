27172fa6ac05c83dc1fd5a8a592f720144aeaa13
Iri <89229099+IAgretti@users.noreply.github.com>
2023-11-02 09:49:06 -0300

    Clase abstracta y virtual

---
+++ Clase25R/Clase25R/BaseClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase25R
+{
+    public abstract class BaseClass
+    {
+        public abstract double Area(double radio);
+        public virtual double InteresMensual(double cantidad)
+        {
+            return cantidad * 12 / 100;
+        }
+        public virtual double CantidadTotal(double cant, double cantPpal)
+        {
+            return cant + cantPpal;
+        }
+    }
+    public class DeriveClass: BaseClass
+    {
+        public override double Area(double radio)
+        {
+            return 2 * 3.14 * radio;
+        }
+        public override double InteresMensual(double cantidad)
+        {
+            return cantidad * 14 / 100;
+        }
+    }
+}
---
+++ Clase25R/Clase25R/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Clase25R
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass bc = new DeriveClass();
+            double r = bc.InteresMensual(12);
+            Console.WriteLine("El interés es de: " + r);
+            double ct = bc.CantidadTotal(10, 5);
+            Console.WriteLine("La cantidad total mensual es de: " + ct);
+            double area = bc.Area(6);
+            Console.WriteLine("El área es de: " + area);
+
+            DeriveClass b = new DeriveClass();
+            double a = b.Area(3);
+            Console.WriteLine("El área de la clase derivada es: " + a);
+            double e = b.InteresMensual(12);
+            Console.WriteLine("El interés generado en la clase derivada es: " + e);
+
+
+            Console.ReadKey();
+        }
+    }
+}
