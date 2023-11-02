70cb10df83046ed4ebe8b64bea8f8b2157545e1d
Iri <89229099+IAgretti@users.noreply.github.com>
2023-11-02 09:51:47 -0300

    Clase abstracta traductor y ejemplo color consola

---
+++ Clase25A/Clase25A/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Clase25A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, direccion, telefono;
+            Console.Title = "Datos del alumno";
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.SetCursorPosition(6, 2);
+            Console.Write("Nombre: ");
+            Console.SetCursorPosition(6, 4);
+            Console.Write("Dirección: ");
+            Console.SetCursorPosition(6, 6);
+            Console.Write("Teléfono: ");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.SetCursorPosition(18, 2);
+            nombre = Console.ReadLine();
+            Console.SetCursorPosition(18, 4);
+            direccion = Console.ReadLine();
+            Console.SetCursorPosition(18, 6);
+            telefono = Console.ReadLine();
+
+            //Traductor english = new InglesT();
+            //Traductor french = new FrancesT();
+            //Traductor german = new AlemanT();
+
+            //english.Print();
+            //french.Print();
+            //german.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase25A/Clase25A/Traductor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase25A
+{
+    public abstract class Traductor
+    {
+        public abstract string Lenguaje();
+        public void Print()
+        {
+            Console.WriteLine("Soy traductor de: " + Lenguaje());
+        }
+    }
+    public class AlemanT: Traductor
+    {
+        public override string Lenguaje()
+        {
+            return "Alemán!";
+        }
+    }
+    public class InglesT: Traductor
+    {
+        public override string Lenguaje()
+        {
+            return "Inglés";
+        }
+    }
+    public class FrancesT: Traductor
+    {
+        public override string Lenguaje()
+        {
+            return "Francés!";
+        }
+    }
+}
